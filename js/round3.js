f65fb63b476557181cb2473b2b9ddbc99e96fedc
bo0mero0 <pbtruong@gmail.com>
2016-10-24 16:31:21 -0400

    took out shield movements

--- js/round3.js
+++ js/round3.js
@@ -85,11 +85,11 @@ let round3 = function() {
   World.add(phisyxEngine.world, [background, shield, platform1, platform2, platform3, p, h, i, rock, sling, ceiling, wallL, wallR, ground, dragMouse]);
 
 
-  Events.on(phisyxEngine, 'beforeUpdate', function(event) {
-    let py = 300 + 150 * Math.sin(phisyxEngine.timing.timestamp * 0.002);
-    Body.setVelocity(shield, { x: 0, y: py - shield.position.y });
-    Body.setPosition(shield, { x: 1000, y: py - 50 });
-  })
+  // Events.on(phisyxEngine, 'beforeUpdate', function(event) {
+  //   let py = 300 + 150 * Math.sin(phisyxEngine.timing.timestamp * 0.002);
+  //   Body.setVelocity(shield, { x: 0, y: py - shield.position.y });
+  //   Body.setPosition(shield, { x: 1000, y: py - 50 });
+  // })
   Events.on(phisyxEngine, 'afterUpdate', function() {
     if (dragMouse.mouse.button === -1 && (rock.position.x > 350 || rock.position.y < 380)) {
       let rock2 = rock;
