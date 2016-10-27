99972b60d6d25cfb50a2dce4df15f4720bc53778
bo0mero0 <pbtruong@gmail.com>
2016-10-27 17:13:18 -0400

    add countdown for round 4

--- js/round4.js
+++ js/round4.js
@@ -31,152 +31,167 @@ let startTimeout;
 
 let round4 = function() {
   let gameover = false;
-dragMouse = MouseConstraint.create(phisyxEngine);
-phisyxEngine.render.bounds.max = {x: 800, y: 500};
-phisyxEngine.render.bounds.min = {x: 0, y: 0};
-phisyxEngine.render.options.background= './images/round4background.jpg';
+  dragMouse = MouseConstraint.create(phisyxEngine);
+  phisyxEngine.render.bounds.max = {x: 800, y: 500};
+  phisyxEngine.render.bounds.min = {x: 0, y: 0};
+  phisyxEngine.render.options.background= './images/round4background.jpg';
 
+  let countdown1 = Bodies.circle(600, 100, 72, {isStatic: true, mass: 1,name: "countdown", collisionFilter: {mask: 0x0001 | 0x0004}, render:  {sprite: {xScale: .4, yScale: .4, texture: './images/countdown1.png'}}})
+  let countdown2 = Bodies.circle(400, 100, 72, {isStatic: true, mass: 1,name: "countdown", collisionFilter: {mask: 0x0001 | 0x0004}, render:  {sprite: {xScale: .4, yScale: .4, texture: './images/countdown2.png'}}})
+  let countdown3 = Bodies.circle(200, 100, 72, {isStatic: true, mass: 1,name: "countdown", collisionFilter: {mask: 0x0001 | 0x0004}, render:  {sprite: {xScale: .4, yScale: .4, texture: './images/countdown3.png'}}})
 
+  let ground = Bodies.rectangle(400, 500, 810, 5, { name: "ground", isStatic: true, render: {opacity: 0} });
+  let ceiling = Bodies.rectangle(400, 0, 800, 5, { isStatic: true, render: {opacity: 0} });
+  let wallL = Bodies.rectangle(0, 300, 5, 600, { isStatic: true, render: {opacity: 0} });
+  let wallR = Bodies.rectangle(800, 300, 5, 600, { isStatic: true, render: {opacity: 0} });
+  ground.collisionFilter.category = 0x0002 | 0x0004;
 
-let ground = Bodies.rectangle(400, 500, 810, 5, { name: "ground", isStatic: true, render: {opacity: 0} });
-let ceiling = Bodies.rectangle(400, 0, 800, 5, { isStatic: true, render: {opacity: 0} });
-let wallL = Bodies.rectangle(0, 300, 5, 600, { isStatic: true, render: {opacity: 0} });
-let wallR = Bodies.rectangle(800, 300, 5, 600, { isStatic: true, render: {opacity: 0} });
-ground.collisionFilter.category = 0x0002 | 0x0004;
-
-function createLetter(x, y, key) {
-	return Body.create({
-		position: {
-			x: x,
-			y: y
-		},
-		vertices: JSON.parse(JSON.stringify(letters[key].vertices)),
-		mass: 0.0017,
-		friction: 0,
-		restitution: 1
-	});
-}
-
-var letterBodies = [];
-
-function addLetter(letter) {
-  let letterBody = Body.create({
-                        name: "gameLetter",
-                        letterType: "shooting",
-                        letterChar: letter,
-                        letter: letter,
-                        render: {
-                          fillStyle: "#ffffff",
-                          strokeStyle: "#ffffff"
-                        },
-                    		position: {
-                    			x: phisyxEngine.render.options.width / 2 + letters[letter].pos,
-                    			y: 430
-                    		},
-                    		vertices: JSON.parse(JSON.stringify(letters[letter].vertices)),
-                    		mass: 0.0017,
-                    		friction: 0,
-                    		restitution: 1
-  });
-  // letterBody.collisionFilter.group = letters[letter].group;
-  // letterBody.collisionFilter.mask = letters[letter].mask;
-  // letterBody.collisionFilter.category = letters[letter].mask;
-  Body.scale(letterBody, .5, .5);
-  // letterBody.angle = Math.random() * 0.5 - 0.25;
-  letterBody.force.y -= 0.00003;
-  letterBodies.push(letterBody);
-  World.add(phisyxEngine.world, letterBodies[letterBodies.length - 1]);
-
-  if (letterBodies.length > 30) {
-    World.remove(phisyxEngine.world, letterBodies[0]);
-    letterBodies = letterBodies.slice(1);
+  function createLetter(x, y, key) {
+  	return Body.create({
+  		position: {
+  			x: x,
+  			y: y
+  		},
+  		vertices: JSON.parse(JSON.stringify(letters[key].vertices)),
+  		mass: 0.0017,
+  		friction: 0,
+  		restitution: 1
+  	});
   }
-}
 
-World.add(phisyxEngine.world, [ceiling, wallL, wallR, ground, dragMouse]);
+  var letterBodies = [];
 
-$(document).on("keypress", function(e) {
-  var key = String.fromCharCode(e.which).toUpperCase();
-  if(Object.keys(letters).includes(key)){
-    addLetter(key);
-  }
-});
-
-// $(".key").bind("click touch",function(e) {
-//   e.preventDefault();
-//   var key = String.fromCharCode($(this).attr("id").charCodeAt(0)).toUpperCase();
-//   if(key in letters){
-//     addLetter(key);
-//   }
-// });
-let time = 700;
-
-let createFallingLetter = () => {
-    time -= 3;
-    let letterArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    let randLetter = letterArr[Math.floor(Math.random() * 25)];
-    let fallingLetter = Body.create({
+  function addLetter(letter) {
+    let letterBody = Body.create({
                           name: "gameLetter",
-                          letterType: "falling",
-                          letterChar: randLetter,
+                          letterType: "shooting",
+                          letterChar: letter,
+                          letter: letter,
+                          render: {
+                            fillStyle: "#ffffff",
+                            strokeStyle: "#ffffff"
+                          },
                       		position: {
-                      			x: phisyxEngine.render.options.width / 2 + letters[randLetter].pos,
-                      			y: 30
+                      			x: phisyxEngine.render.options.width / 2 + letters[letter].pos,
+                      			y: 430
                       		},
-                      		vertices: JSON.parse(JSON.stringify(letters[randLetter].vertices)),
-                      		mass: 10000,
+                      		vertices: JSON.parse(JSON.stringify(letters[letter].vertices)),
+                      		mass: 0.0017,
                       		friction: 0,
                       		restitution: 1
     });
-    // fallingLetter.timeScale = .1;
-    Body.scale(fallingLetter, .5, .5);
-    World.add(phisyxEngine.world, fallingLetter);
-    window.setTimeout(() => {
-      fallingLetter.timeScale = .1
-      // debugger
-       console.log("hello");}, 100)
+    // letterBody.collisionFilter.group = letters[letter].group;
+    // letterBody.collisionFilter.mask = letters[letter].mask;
+    // letterBody.collisionFilter.category = letters[letter].mask;
+    Body.scale(letterBody, .5, .5);
+    // letterBody.angle = Math.random() * 0.5 - 0.25;
+    letterBody.force.y -= 0.00003;
+    letterBodies.push(letterBody);
+    World.add(phisyxEngine.world, letterBodies[letterBodies.length - 1]);
+
+    if (letterBodies.length > 30) {
+      World.remove(phisyxEngine.world, letterBodies[0]);
+      letterBodies = letterBodies.slice(1);
+    }
   }
 
-startTimeout = window.setTimeout(() => {
-  timeInterval = window.setInterval(createFallingLetter, time);
-}, 3000);
+  World.add(phisyxEngine.world, [ceiling, wallL, wallR, ground, countdown1, countdown2, countdown3]);
 
-let score = 0
-let recentHighScore;
-Events.on(phisyxEngine, "collisionStart", (e) => {
-  if (gameover === false && recentHighScore !== score && time - (score * 5) > 50 && score % 10 === 0 ) {
-    recentHighScore = score;
-    window.clearInterval(timeInterval);
-    timeInterval = window.setInterval(createFallingLetter, time - (score * 5));
-  }
-  if (e.pairs[0].bodyA.name === "ground" && e.pairs[0].bodyB.letterType === "falling") {
-    window.clearInterval(timeInterval);
-    World.clear(phisyxEngine.world, false);
-    gameover = true
-    score = 0;
-  } else if (e.pairs[0].bodyA.name === "ground" && e.pairs[0].bodyB.letterType === "shooting") {
-    $('.score').text(`Score: ${score -= 2}`);
-    World.remove(phisyxEngine.world, [e.pairs[0].bodyB]);
-  } else {
-    if (e.pairs[0].bodyA.name === "gameLetter" && e.pairs[0].bodyB.name === "gameLetter") {
-      if (e.pairs[0].bodyA.letterType !== e.pairs[0].bodyB.letterType && e.pairs[0].bodyA.letterChar === e.pairs[0].bodyB.letterChar) {
-        World.remove(phisyxEngine.world, [e.pairs[0].bodyA, e.pairs[0].bodyB]);
-        $('.score').text(`Score: ${score += 1}`);
-        // window.setTimeout(() => {
-        //   roundEnd = false;
-        //     round2();
-        // }, 3000);
-      } else {
-        if (e.pairs[0].bodyA.letterType === "shooting") {
-          e.pairs[0].bodyA.collisionFilter.category = 0x0001;
-          World.remove(phisyxEngine.world, [e.pairs[0].bodyA]);
-        } else if (e.pairs[0].bodyB.letterType === "shooting") {
-          e.pairs[0].bodyB.collisionFilter.category = 0x0001;
-          World.remove(phisyxEngine.world, [e.pairs[0].bodyB]);
+  $(document).on("keypress", function(e) {
+    var key = String.fromCharCode(e.which).toUpperCase();
+    if(Object.keys(letters).includes(key)){
+      addLetter(key);
+    }
+  });
+
+  // $(".key").bind("click touch",function(e) {
+  //   e.preventDefault();
+  //   var key = String.fromCharCode($(this).attr("id").charCodeAt(0)).toUpperCase();
+  //   if(key in letters){
+  //     addLetter(key);
+  //   }
+  // });
+  let time = 700;
+
+  let createFallingLetter = () => {
+      time -= 3;
+      let letterArr = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      let randLetter = letterArr[Math.floor(Math.random() * 25)];
+      let fallingLetter = Body.create({
+                            name: "gameLetter",
+                            letterType: "falling",
+                            letterChar: randLetter,
+                        		position: {
+                        			x: phisyxEngine.render.options.width / 2 + letters[randLetter].pos,
+                        			y: 30
+                        		},
+                        		vertices: JSON.parse(JSON.stringify(letters[randLetter].vertices)),
+                        		mass: 10000,
+                        		friction: 0,
+                        		restitution: 1
+      });
+      // fallingLetter.timeScale = .1;
+      Body.scale(fallingLetter, .5, .5);
+      World.add(phisyxEngine.world, fallingLetter);
+      window.setTimeout(() => {
+        fallingLetter.timeScale = .01;
+        }, 100)
+    }
+
+
+  startTimeout = window.setTimeout(() => {
+    countdown3.isStatic = false;
+    startTimeout = window.setTimeout(() => {
+      countdown2.isStatic = false;
+      startTimeout = window.setTimeout(() => {
+        countdown1.isStatic = false;
+        startTimeout = window.setTimeout(() => {
+          World.remove(phisyxEngine.world, [countdown3, countdown2, countdown1]);
+          window.clearInterval(timeInterval);
+          timeInterval = window.setInterval(createFallingLetter, time);
+        }, 1500);
+      }, 1000);
+    }, 1000);
+
+  }, 1000);
+
+  let score = 0
+  let recentHighScore = 0;
+  Events.on(phisyxEngine, "collisionStart", (e) => {
+    console.log(gameover);
+    if (gameover === false && recentHighScore !== score && time - (score * 5) > 50 && score % 10 === 0 ) {
+      recentHighScore = score;
+      window.clearInterval(timeInterval);
+      timeInterval = window.setInterval(createFallingLetter, time - (score * 5));
+    }
+    if (e.pairs[0].bodyA.name === "ground" && e.pairs[0].bodyB.letterType === "falling") {
+      gameover = true
+      window.clearInterval(timeInterval);
+      World.clear(phisyxEngine.world, false);
+      score = 0;
+    } else if (e.pairs[0].bodyA.name === "ground" && e.pairs[0].bodyB.letterType === "shooting") {
+      $('.score').text(`Score: ${score -= 2}`);
+      World.remove(phisyxEngine.world, [e.pairs[0].bodyB]);
+    } else {
+      if (e.pairs[0].bodyA.name === "gameLetter" && e.pairs[0].bodyB.name === "gameLetter") {
+        if (e.pairs[0].bodyA.letterType !== e.pairs[0].bodyB.letterType && e.pairs[0].bodyA.letterChar === e.pairs[0].bodyB.letterChar) {
+          World.remove(phisyxEngine.world, [e.pairs[0].bodyA, e.pairs[0].bodyB]);
+          $('.score').text(`Score: ${score += 1}`);
+          // window.setTimeout(() => {
+          //   roundEnd = false;
+          //     round2();
+          // }, 3000);
+        } else {
+          if (e.pairs[0].bodyA.letterType === "shooting") {
+            e.pairs[0].bodyA.collisionFilter.category = 0x0001;
+            World.remove(phisyxEngine.world, [e.pairs[0].bodyA]);
+          } else if (e.pairs[0].bodyB.letterType === "shooting") {
+            e.pairs[0].bodyB.collisionFilter.category = 0x0001;
+            World.remove(phisyxEngine.world, [e.pairs[0].bodyB]);
+          }
         }
       }
     }
-  }
-});
+  });
 
 };
